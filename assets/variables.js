9aaa002401fb41d16b2efd6d01b996f8487f32fc
HeiBroo <67837556+HeiBroo@users.noreply.github.com>
2020-07-05 00:26:52 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,5 +1,16 @@
 export default {
-  nicknames: ["Ava", "Babe", "Sayang", "Avakuu", "Cintuu"],
+  nicknames: ["141 Diamond = 33.000
+282 Diamond = 57.000
+345 Diamond = 70.000
+415 Diamond = 90.000
+708 Diamond = 135.000
+1446 Diamond = 280.000
+2976 Diamond = 550.000
+
+Via id (server)
+legal 1000000%
+
+payment Ovo,dana"],
   greetings: {
     evening: "Good Evening",
     afternoon: "Good Afternoon",
